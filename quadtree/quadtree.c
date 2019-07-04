b207c4c2be54fc25744501acaaad20688f0a8ce8
Pedro Schneider <pedroschneider@outlook.com>
2019-07-03 23:44:00 -0300

    small refactor

--- quadtree/quadtree.c
+++ quadtree/quadtree.c
@@ -36,12 +36,12 @@ int findindex(Img* pic, int x, int y) {
 void geraQuad(Img* pic, float minDetail, QuadNode* node) {
     // calculo da area da regiao:
     int reg_size = 3 * node->width * node->height;
-    
+
     // calculo da cor media de cada componente RGB
     float rm = 0.0;
     float gm = 0.0;
     float bm = 0.0;
-    
+
     for (int i = node->x; i < node->width * 3; i += 3) {
         for (int j = node->y; j < node->height; j++) {
             int indexRed = findindex(pic, i, j);
@@ -53,7 +53,7 @@ void geraQuad(Img* pic, float minDetail, QuadNode* node) {
     rm = rm / reg_size;
     gm = gm / reg_size;
     bm = bm / reg_size;
-    
+
     // calculo da distancia euclidiana das componentes e nivel do detalhe
     float dr, dg, db;
     float detalhe = 0.0;
@@ -71,7 +71,7 @@ void geraQuad(Img* pic, float minDetail, QuadNode* node) {
         }
     }
     detalhe = detalhe / reg_size;
-    
+
     // caso de parada da possivel recursao
     if (detalhe > minDetail) {
         // atualiza o nodo atual
@@ -79,7 +79,7 @@ void geraQuad(Img* pic, float minDetail, QuadNode* node) {
         node->color[0] = 0;
         node->color[1] = 255;
         node->color[2] = 0;
-        
+
         // divide o quadrante <=== nao sei aida se x e y estao certos!
         int x = node->x;
         int y = node->y;
@@ -91,13 +91,13 @@ void geraQuad(Img* pic, float minDetail, QuadNode* node) {
         QuadNode* sw = newNode(x, y, node->width / 2, node->height / 2);
         x = x + 3 * node->width / 2;
         QuadNode* se = newNode(x, y, node->width / 2, node->height / 2);
-        
+
         //chama recursivamente
         geraQuad(pic, minDetail, nw);
         geraQuad(pic, minDetail, ne);
         geraQuad(pic, minDetail, sw);
         geraQuad(pic, minDetail, se);
-        
+
         // linkando
         node->NW = nw;
         node->NE = ne;
@@ -115,42 +115,42 @@ void geraQuad(Img* pic, float minDetail, QuadNode* node) {
 QuadNode* geraQuadtree(Img* pic, float minDetail) {
     int width = pic->width;
     int height = pic->height;
-    
+
     QuadNode* raiz = newNode(0, 0, width, height);
     geraQuad(pic, minDetail, raiz);
     return raiz;
-    
+
     //#define DEMO
 #ifdef DEMO
-    
+
     /************************************************************/
     /* Teste: criando uma raiz e dois nodos a mais              */
     /************************************************************/
-    
+
     QuadNode* raiz = newNode(0, 0, width, height);
     raiz->status = PARCIAL;
     raiz->color[0] = 0;
     raiz->color[1] = 0;
     raiz->color[2] = 255;
-    
+
     QuadNode* nw = newNode(width / 2, 0, width / 2, height / 2);
     nw->status = PARCIAL;
     nw->color[0] = 0;
     nw->color[1] = 0;
     nw->color[2] = 255;
-    
+
     // Aponta da raiz para o nodo nw
     raiz->NW = nw;
-    
+
     QuadNode* nw2 = newNode(width / 2 + width / 4, 0, width / 4, height / 4);
     nw2->status = CHEIO;
     nw2->color[0] = 255;
     nw2->color[1] = 0;
     nw2->color[2] = 0;
-    
+
     // Aponta do nodo nw para o nodo nw2
     nw->NW = nw2;
-    
+
 #endif
     // Finalmente, retorna a raiz da árvore
     return raiz;
@@ -208,9 +208,9 @@ void writeNode(FILE* fp, QuadNode* n) {
 // Desenha todos os nodos da quadtree, recursivamente
 void drawNode(QuadNode* n) {
     if (n == NULL) return;
-    
+
     glLineWidth(0.1);
-    
+
     if (n->status == CHEIO) {
         glBegin(GL_QUADS);
         glColor3ubv(n->color);
@@ -220,7 +220,7 @@ void drawNode(QuadNode* n) {
         glVertex2f(n->x, n->y + n->height - 1);
         glEnd();
     }
-    
+
     else if (n->status == PARCIAL) {
         if (desenhaBorda) {
             glBegin(GL_LINE_LOOP);
